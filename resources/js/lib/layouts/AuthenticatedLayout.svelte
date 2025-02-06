8281d77520f2591b9dd390d0131bb49e43b0cce9
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-02-06 17:47:36 -0300

    replaced layouts

---
+++ resources/js/lib/layouts/AuthenticatedLayout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import AppBreadcrumb from '$lib/components/ui/custom/app-breadcrumb.svelte';
+    import AppCommand from '$lib/components/ui/custom/app-command.svelte';
+    import AppSidebar from '$lib/components/ui/custom/app-sidebar.svelte';
+    import { Separator } from '$lib/components/ui/separator';
+    import {
+        SidebarInset,
+        SidebarProvider,
+        SidebarTrigger,
+    } from '$lib/components/ui/sidebar';
+    import { Toaster } from '$lib/components/ui/sonner';
+    import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<ModeWatcher />
+<SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+        <header
+            class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+        >
+            <div class="flex items-center gap-2 px-4">
+                <SidebarTrigger class="-ml-1" />
+                <Separator orientation="vertical" class="mr-2 h-4" />
+                <AppBreadcrumb />
+            </div>
+        </header>
+        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <slot />
+        </div>
+    </SidebarInset>
+    <Toaster position="top-right" />
+    <AppCommand />
+</SidebarProvider>
---
+++ resources/js/lib/layouts/AuthenticationLayout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { Link } from '@inertiajs/svelte';
+    import { Command } from 'lucide-svelte';
+    import type { Snippet } from 'svelte';
+
+    type Props = {
+        children?: Snippet;
+    };
+
+    let { children }: Props = $props();
+</script>
+
+<div class="grid min-h-svh">
+    <div class="flex flex-col gap-4 bg-background p-6 md:p-10">
+        <div class="flex justify-center gap-2 md:justify-start">
+            <Link href="/" class="flex items-center gap-2 font-medium">
+                <div
+                    class="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground"
+                >
+                    <Command class="size-4" />
+                </div>
+                Svelte Inertia
+            </Link>
+        </div>
+        <div class="flex flex-1 items-center justify-center">
+            <div class="w-full max-w-xs">
+                {@render children?.()}
+            </div>
+        </div>
+    </div>
+</div>
